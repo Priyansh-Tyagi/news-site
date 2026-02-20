4d03a7abe9204aec3ec5e3d064e3c17482d1d715
Priyansh-Tyagi <146009781+Priyansh-Tyagi@users.noreply.github.com>
2026-02-20 15:49:25 +0530

    Change news API base URL to newsdata.io

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,5 +1,5 @@
 const API_KEY = "pub_7e9afab256f54419b9d78e91a3bdc6b0";
-const BASE_URL = "https://newsapi.org/v2/top-headlines";
+const BASE_URL = "https://newsdata.io/api/1/latest?";
 
 export const fetchNewsByCategory = async (category = "") => {
   const url = `${BASE_URL}?country=us&category=${category}&apiKey=${API_KEY}`;
