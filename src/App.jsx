91205c0b1488d3252141a74aee8e953d5ce99015
Priyansh-Tyagi <146009781+Priyansh-Tyagi@users.noreply.github.com>
2026-02-20 15:06:38 +0530

    Fix import case for Styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import CategoryPage from "./components/CategoryPage";
-import "./styles.css";
+import "./Styles.css";
 
 export default function App() {
   return (
