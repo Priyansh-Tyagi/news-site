50091c16b8efc2ad182b9560225e455f4b7439f1
Priyansh-Tyagi <146009781+Priyansh-Tyagi@users.noreply.github.com>
2026-02-20 15:52:18 +0530

    Update news API URL and response handling

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,11 +1,11 @@
 const API_KEY = "pub_7e9afab256f54419b9d78e91a3bdc6b0";
-const BASE_URL = "https://newsdata.io/api/1/latest?";
+const BASE_URL = "https://newsdata.io/api/1/latest";
 
 export const fetchNewsByCategory = async (category = "") => {
-  const url = `${BASE_URL}?country=us&category=${category}&apiKey=${API_KEY}`;
+  const url = `${BASE_URL}?country=us&category=${category}&apikey=${API_KEY}`;
 
   const res = await fetch(url);
   const data = await res.json();
 
-  return data.articles || [];
+  return data.results || [];
 };
